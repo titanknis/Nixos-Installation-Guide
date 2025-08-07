2a9146fc076c0b0adf6018fe7b487ac04cac2705
titanknis <titanknis@gmail.com>
2025-08-07 21:31:35 +0100

    change(boot): switch from GRUB to systemd-boot and clean up config
    systemd-boot integrates well with NixOS generations, offers faster boot
    times, and is simpler.
    Also performed minor configuration cleanup.

--- configuration.nix
+++ configuration.nix
@@ -2,60 +2,31 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
 
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
   # luks setup
-  boot.initrd = {
-      luks.devices = {
-        luksCrypted = {
-          device = "/dev/nvme0n1p3"; # Replace with your UUID
-          preLVM = true; # Unlock before activating LVM
-          allowDiscards = true; # Allow TRIM commands for SSDs
-        };
-      };
-      #lvm.devices = [ "/dev/mapper/luksCrypted" ]; # Activate LVM after decryption
-  };
-
-  # Use the GRUB bootloader
-  boot.loader.grub.enable = true;  # Enable GRUB as the bootloader
-  boot.loader.grub.device = "nodev";  # Install GRUB on the EFI system partition
-  boot.loader.grub.copyKernels = true;  # Activate automatic copying of kernel files
-  boot.loader.grub.efiSupport = true;  # Enable EFI support for GRUB
-  boot.loader.grub.enableCryptodisk = true ; # Enable GRUB support for encrypted disks
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";  # Mount point of the EFI system partition
-  boot.loader.efi.canTouchEfiVariables = true;  # Allow GRUB to modify EFI variables for boot entry management
-  
-  # Adds custom menu entries for reboot and poweroff
-  boot.loader.grub.extraEntries = ''
-      menuentry "Reboot" {
-          reboot
-      }
-      menuentry "Poweroff" {
-          halt
-      }
-  '';
-
-  # Specify the Zen kernel
-  boot.kernelPackages = pkgs.linuxPackages_zen;
-
-  # Add GRUB theme (example using 'sleek-grub-theme')
-  # boot.loader.grub.sleekTheme.enable = true;
-  # boot.loader.grub.sleekTheme.themeName = "dark";  # Replace with the desired theme name ('dark', 'light', 'orange', 'bigsur')
-
-
-
+  boot.initrd.luks.devices.luksCrypted.device = "/dev/nvme0n1p2";
+  boot.initrd.luks.devices.luksCrypted.allowDiscards = true; # Allow TRIM commands for SSDs
 
   networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
 
+  # Pick only one of the below networking options.
+  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Africa/Tunis";
@@ -65,47 +36,29 @@
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  # i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
-  #  keyMap = "us-colemak-dh";
+    # keyMap = "mod-dh-ansi-us";
     useXkbConfig = true; # use xkb.options in tty.
   };
 
-  # Enable the X11 windowing system
-  services.xserver.enable = true;
-
   # Set XKB keyboard layout options
   services.xserver.xkb = {
-    layout = "us";                          # Set the keyboard layout to US
-    variant = "colemak_dh";                 # Use the Colemak DH variant
-    options = "caps:escape, compose:ralt, terminate:ctrl_alt_bksp";  # Set additional XKB options
-    # options = "misc:extend,lv5:caps_switch_lock,compose:menu";  # Alternative XKB options (commented out)
+    layout = "us"; # Set the keyboard layout to US
+    variant = "colemak_dh"; # Use the Colemak DH variant
+    # options = "eurosign:e,caps:escape";
   };
 
   # Enable the KDE Plasma desktop environment
   services.desktopManager.plasma6.enable = true;
 
-  # Enable Wayland for KDE Plasma
-  # services.xserver.windowManager.plasma6.enableWayland = true;
-
   # Enable the SDDM display manager
   services.displayManager.sddm.enable = true;
-  services.displayManager.defaultSession = "plasma";  # Set the default session to Plasma Wayland
-  # services.displayManager.autoLogin.enable = true;  # Enable auto-login (commented out)
-  # services.displayManager.autoLogin.user = "alice";  # Set the auto-login user to 'alice' (commented out)
-
-  # Set GTK/Qt themes
-  #qt.enable = true;                 # Enable Qt integration
-  #qt.platformTheme = "gtk2";        # Set the platform theme to GTK2
-  #qt.style = "gtk2";                # Set the style to GTK2
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.defaultSession = "plasma"; # Set the default session to Plasma Wayland
 
   # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
   services.pipewire = {
     enable = true;
     pulse.enable = true;
@@ -114,40 +67,41 @@
   # Enable touchpad support (enabled default in most desktopManager).
   services.libinput.enable = true;
 
-
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
 
   # Set the default shell for all users to zsh
-  # users.defaultShell = pkgs.zsh;
+  programs.zsh.enable = true;
+  programs.zsh.autosuggestions.enable = true;
+  programs.zsh.syntaxHighlighting.enable = true;
+  programs.starship.enable = true;
+  users.defaultUserShell = pkgs.zsh;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.titanknis = {
     isNormalUser = true;
     home = "/home/titanknis";
-    description = "The system master";
-    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
-  #   openssh.authorizedKeys.keys = [ "ssh-dss AAAAB3Nza... alice@foobar" ];
+    extraGroups = [
+      "wheel" # Enable ‘sudo’ for the user.
+      "networkmanager"
+    ];
+    # packages = with pkgs; [
+    #   tree
+    # ];
 
-  #   packages = with pkgs; [
-  #     firefox
-  #     tree
-  #   ];
+    # openssh.authorizedKeys.keys = [ "ssh-dss AAAAB3Nza... alice@foobar" ];
   };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    firefox # favorite browser
-    kitty 
+    firefox # browser
+    kitty # terminal
     neovim # editor of choice
-    wget # wget is better than curl because it will resume with exponential backoff
-    curl # curl is better than wget because it supports more protocols
-    git # git version control
   ];
-
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
-  # programs.mtr.enable = true;
   programs.gnupg.agent = {
     enable = true;
     enableSSHSupport = true;
@@ -163,11 +117,6 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
@@ -186,7 +135,6 @@
   # and migrated your data accordingly.
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "25.05"; # Did you read the comment?
 
 }
-
